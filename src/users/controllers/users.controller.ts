213b2128dc9d00a1db1aef0e795f76924770242b
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2025-02-10 23:04:12 -0600

    resolved roles guard merge conflict

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <<<<<<< HEAD:src/users/controllers/users.controller.ts
 import { Controller, Get, Post, Body, Param, Request } from '@nestjs/common';
 import { CreateUserDto } from '../dto/create-user.dto';
@@ -17,10 +18,12 @@ import {
   UseGuards,
   Request,
 } from '@nestjs/common';
+=======
+import { Controller, Get, Post, Body, Param, Request } from '@nestjs/common';
+>>>>>>> 10f4f11 (HSA-52: Add role based Authorization for a house)
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UserBaseEntity, UserDetailEntity } from '../entities/user.entity';
 import { ApiBearerAuth, ApiOkResponse } from '@nestjs/swagger';
-import { AuthGuard, Public } from '../../guards/auth.guard';
 import { UsersService } from '../services/users.service';
 >>>>>>> 18ad784 (HSA-51: Add Auth Guard & Swagger Authorization):src/users/users.controller.ts
 
