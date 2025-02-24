8c63b90751d954c5e2114945f02c77a7c67ed853
Sebastian Gomes <romy6047@gmail.com>
2025-02-23 21:00:57 -0600

    HSA54: sign up updated
    Currently the email password signup has the following steps:
    
    a. app creates user in firebase
    b. app asks firebase to send varification link
    c. app asks our nest backend to create user linked with that firebase account
    e. then user logs in
    
    I am changing that and making it more direct. Now the app will ask for signup in our backend and the our nest backend will communicate with firebase to do the rest.

--- src/users/services/auth.service.ts
+++ src/users/services/auth.service.ts
@@ -1,11 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/data/services/prisma/prisma.service';
 import { User } from '@prisma/client';
 import { FirebaseClientService } from 'src/data/services/firebase/firebase-client.service';
 import { loginDto, SignupDto } from '../dto/auth.dto';
 import { FirebaseAdminService } from 'src/data/services/firebase/firebase-admin.service';
 import {
+  createUserWithEmailAndPassword,
   getAuth,
+  sendEmailVerification,
   signInWithEmailAndPassword,
   UserCredential,
 } from 'firebase/auth';
@@ -19,10 +21,31 @@ export class AuthService {
   ) {}
   async signup(signupDto: SignupDto): Promise<User> {
     // Check if the user already exists
+    const existingUser = await this.prismaService.user.findUnique({
+      where: { email: signupDto.email },
+    });
+
+    if (existingUser) {
+      throw new BadRequestException('User with this email already exists');
+    }
+
     console.log(signupDto);
-    const firebaseUser = await this.firebaseAdminService
-      .getAuth()
-      .createUser(signupDto);
+    const auth = this.firebaseClientService.getAuth();
+    const firebaseUser = await createUserWithEmailAndPassword(
+      auth,
+      signupDto.email,
+      signupDto.password,
+    )
+      .then(async function (userResponse) {
+        console.log('User successfully created:', userResponse.user.uid);
+        await sendEmailVerification(userResponse.user);
+        return userResponse.user;
+      })
+      .then(function (user) {
+        console.log('Email verification email successfully sent!');
+        return user;
+      });
+
     const user = await this.prismaService.user.create({
       data: {
         auth_id: firebaseUser.uid,
